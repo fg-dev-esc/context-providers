101d0112d87ad21fe11f0deb7351dfdb13bfc310
state-of-dev <lfgxxo@gmail.com>
2026-08-10 01:25:23 -0600

    feat: add initial setup for Tailwind CSS and PostCSS with global styles

---
+++ dynamic-rain-website/app/page.tsx
@@ -0,0 +1,9 @@
+import MatrixRain from "@/components/matrix-rain"
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen bg-black overflow-hidden">
+      <MatrixRain />
+    </main>
+  )
+}
